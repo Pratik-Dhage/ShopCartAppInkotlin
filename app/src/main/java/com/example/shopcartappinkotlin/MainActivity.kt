4e1a8078015c5c44282debee17d8d4209c1479d7
Pratik Dhage <dhagepratik94@gmail.com>
2022-12-26 16:01:59 +0530

    bottom nav setup with fragment successful

--- app/src/main/java/com/example/shopcartappinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/shopcartappinkotlin/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.shopcartappinkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.PopupMenu
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.shopcartappinkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var view : View
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        initializeFields()
+        fragmentSetUp()
+        onClickListeners()
+    }
+
+    private fun fragmentSetUp() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomNavBar.setupWithNavController(popupMenu.menu,navController)
+
+        navController.addOnDestinationChangedListener(object : NavController.OnDestinationChangedListener{
+            override fun onDestinationChanged(
+                controller: NavController,
+                destination: NavDestination,
+                arguments: Bundle?
+            ) {
+                title = when(destination.id){
+                    R.id.cartFragment-> resources.getString(R.string.cart)
+                    R.id.moreFragment-> resources.getString(R.string.profile)
+
+                    else ->  resources.getString(R.string.shopcart)
+                }
+            }
+
+        })
+    }
+
+    private fun onClickListeners() {
+       // TODO("Not yet implemented")
+    }
+
+    private fun initializeFields() {
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+       view = binding.root
+
     }
 }
